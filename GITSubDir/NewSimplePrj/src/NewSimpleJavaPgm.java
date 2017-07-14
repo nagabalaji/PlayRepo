06ef0f74ecb5bcb6da72ca19628a7d32747b3331
NagaBalajiBaskaran <emailnaga@gmail.com>
2017-07-14 00:06:55 -0400

    Previous commit is amended

--- GITSubDir/NewSimplePrj/src/NewSimpleJavaPgm.java
+++ GITSubDir/NewSimplePrj/src/NewSimpleJavaPgm.java
@@ -3,7 +3,7 @@ public class NewSimpleJavaPgm {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test");
+		System.out.println("Test1");
 	}
 
 }
